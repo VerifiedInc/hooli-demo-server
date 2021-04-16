2f75ed306ed2ac873d94ff5f791cc9d61c09e0f9
Ray Smets <rayjsmets@gmail.com>
2021-04-16 16:38:06 -0700

    [ORM] request context added as express middleware. (#3)
    * [Refactor] updated the file naming convention of the new websocket dir to better match the api and data directories. (#18)
    
    * [Presentation] service create response fixed by removing the plaintext presentation. (#19)
    
    * [Presentation] service create response fixed by removing the plaintext presentation.
    
    * [Package] release demo-types version.
    
    * [ORM] request context added as express middleware.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,13 @@
+import { RequestContext } from '@mikro-orm/core';
+import { Request, Response, NextFunction } from 'express';
 import { Application } from '../declarations';
+import logger from '../logger';
 // Don't remove this comment. It's needed to format import lines nicely.
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
 export default function (app: Application): void {
+  app.use((req, res, next) => {
+    // As a fix for .flush validation errors, ref: https://mikro-orm.io/docs/faq/#you-cannot-call-emflush-from-inside-lifecycle-hook-handlers
+    // Having a unique Mikro-ORM request context per request. ref: https://mikro-orm.io/docs/identity-map/#!
+    RequestContext.create(app.get('orm').em, next);
+  });
 }
