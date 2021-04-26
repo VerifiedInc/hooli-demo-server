23e1d1bfb74d982ed6a356fd4bd0834fb7fd41ba
Ray Smets <rayjsmets@gmail.com>
2021-04-26 13:12:47 -0700

    [Migration] fix.

--- src/migrations/Migration20210426174551.ts
+++ src/migrations/Migration20210426174551.ts
@@ -4,7 +4,7 @@ export class Migration20210426174551 extends Migration {
   async up (): Promise<void> {
     this.addSql('alter table "Presentation" add column "verifierDid" varchar(255);');
     const verifierDid = 'did:unum:f2054199-1069-4337-a588-83d099e79d44" where "verifierDid';
-    this.addSql(`update "Presentation" set "verifierDid" = '${verifierDid}' where "verifierDid' is null;`);
+    this.addSql(`update "Presentation" set "verifierDid" = '${verifierDid}' where "verifierDid" is null;`);
     this.addSql('alter table "Presentation" add column "verifierDid" varchar(255) not null;');
   }
 }
