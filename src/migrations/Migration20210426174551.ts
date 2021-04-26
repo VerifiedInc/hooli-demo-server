d84826f12f1de19ac0687bc1c7874f043905a2ed
Ray Smets <rayjsmets@gmail.com>
2021-04-26 13:08:31 -0700

    [Migration] fix.

--- src/migrations/Migration20210426174551.ts
+++ src/migrations/Migration20210426174551.ts
@@ -3,7 +3,8 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20210426174551 extends Migration {
   async up (): Promise<void> {
     this.addSql('alter table "Presentation" add column "verifierDid" varchar(255);');
-    this.addSql('update "Presentation" set "verifierDid" = "did:unum:f2054199-1069-4337-a588-83d099e79d44" where "verifierDid" is null;');
+    const verifierDid = 'did:unum:f2054199-1069-4337-a588-83d099e79d44" where "verifierDid';
+    this.addSql(`update "Presentation" set "verifierDid" = '${verifierDid}' where "verifierDid' is null;`);
     this.addSql('alter table "Presentation" add column "verifierDid" varchar(255) not null;');
   }
 }
