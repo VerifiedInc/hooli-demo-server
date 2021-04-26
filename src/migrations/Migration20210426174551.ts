af477df96e5838f72e58f2a08f4583f7b9c783af
Ray Smets <rayjsmets@gmail.com>
2021-04-26 12:50:53 -0700

    [Migration] setting verifier did .

--- src/migrations/Migration20210426174551.ts
+++ src/migrations/Migration20210426174551.ts
@@ -2,6 +2,7 @@ import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20210426174551 extends Migration {
   async up (): Promise<void> {
+    this.addSql('update "Presentation" set "verifierDid" = "did:unum:f2054199-1069-4337-a588-83d099e79d44" where "verifierDid" is null;');
     this.addSql('alter table "Presentation" add column "verifierDid" varchar(255) not null;');
   }
 }
