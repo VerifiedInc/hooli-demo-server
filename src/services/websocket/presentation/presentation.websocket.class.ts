49a3a6e28e363d0ee1e276de333565ffa7c639a0
Ray Smets <rayjsmets@gmail.com>
2021-06-18 17:36:20 -0700

    [Declined Presentation] fix.

--- src/services/websocket/presentation/presentation.websocket.class.ts
+++ src/services/websocket/presentation/presentation.websocket.class.ts
@@ -1,5 +1,5 @@
 import { Params } from '@feathersjs/feathers';
-import { WithVersion } from '@unumid/types';
+import { PresentationPb, WithVersion } from '@unumid/types';
 import { Service as MikroOrmService } from 'feathers-mikro-orm';
 
 import { Application } from '../../../declarations';
@@ -8,11 +8,13 @@ import { PresentationEntity } from '../../../entities/Presentation';
 import { DemoPresentationDto as DemoPresentationDtoDeprecated } from '@unumid/demo-types-deprecated-v2';
 import { DemoPresentationDto } from '@unumid/demo-types';
 import { lt } from 'semver';
+import { Presentation } from '@unumid/types-deprecated-v2';
+import { isArrayEmpty } from '../../../utils/isArrayEmpty';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ServiceOptions { }
 
-const makeDemoPresentationDtoFromEntity = (entity: WithVersion<PresentationEntity>): DemoPresentationDto => {
+const makeDemoPresentationDtoFromEntity = (entity: WithVersion<PresentationEntity> | PresentationPb | Presentation): DemoPresentationDto => {
   const {
     uuid,
     createdAt,
@@ -24,25 +26,10 @@ const makeDemoPresentationDtoFromEntity = (entity: WithVersion<PresentationEntit
     presentationPresentationRequestId,
     isVerified,
     verifierDid
-  } = entity;
+  } = <WithVersion<PresentationEntity>> entity;
 
-  if (lt(entity.version, '2.0.0')) {
-    return {
-      uuid,
-      createdAt,
-      updatedAt,
-      presentation: {
-        '@context': presentationContext,
-        uuid,
-        type: presentationType,
-        verifiableCredential: presentationVerifiableCredentials,
-        verifierDid,
-        proof: presentationProof,
-        presentationRequestId: presentationPresentationRequestId
-      },
-      isVerified
-    };
-  }
+  // HACK ALERT: to get around new presentations from the mobile sdk still using the deprecated NoPresentation type I am going to mask it here.
+  const maskedType = (entity as PresentationPb).type && (entity as PresentationPb).type.includes('NoPresentation') && isArrayEmpty((entity as PresentationPb).verifiableCredential) ? ['VerifiablePresentation'] : (entity as PresentationPb).type;
 
   return {
     uuid,
@@ -75,7 +62,7 @@ export class PresentationService {
     }
 
     async create (
-      data: WithVersion<PresentationEntity> | NoPresentationEntity,
+      data: WithVersion<PresentationEntity> | Presentation | PresentationPb,
       params?: Params
     ): Promise<DemoPresentationDto | DemoPresentationDtoDeprecated> {
       const response: DemoPresentationDto | DemoPresentationDtoDeprecated = makeDemoPresentationDtoFromEntity(data as WithVersion<PresentationEntity>);
